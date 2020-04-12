4a2668e51bc58474a305fa1ba8a78b55e0aa72b0
Devin Hayward <devin.hayward@icloud.com>
2020-04-12 13:28:17 -0400

    added tests for making moves on the model board

--- Tests/TicTacToeTests/TicTacToeTests.swift
+++ Tests/TicTacToeTests/TicTacToeTests.swift
@@ -1,15 +1,27 @@
 import XCTest
 @testable import TicTacToe
 
+@available(OSX 10.15, *)
 final class TicTacToeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(1, 1)
+   
+    func testBoardMove() {
+        let model = GameModel()
+        
+        // make a move with current player; should be X as default; and check that this registers in the board model
+        model.MBoard.makeMove(location: (1, 1), Cplayer: model.CurrentPlayer)
+        let answer = model.giveMeAString(Cpostion: (1, 1))
+        XCTAssertEqual(answer, "X", "the move did not register in the board")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testBoardMove2() {
+        // initialize the model and make one move
+        let model = GameModel()
+        model.MBoard.makeMove(location: (1, 1), Cplayer: model.CurrentPlayer)
+        
+        // change the current player and check that equality still applies
+        model.CurrentPlayer = TicTacToe.player.y
+        model.MBoard.makeMove(location: (0, 0), Cplayer: model.CurrentPlayer)
+        let answer2 = model.giveMeAString(Cpostion: (0, 0))
+        XCTAssertEqual(answer2, "Y", "the 2nd move did not register in the board")
+    }
 }
